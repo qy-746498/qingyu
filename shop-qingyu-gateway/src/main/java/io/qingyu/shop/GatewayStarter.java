83f364bfb57a9d6509993e9dfd43c6d27bd75f59
qingyu <qy746498@163.com>
2022-05-23 21:02:07 +0800

    初步整合网关

--- shop-qingyu-gateway/src/main/java/io/qingyu/shop/GatewayStarter.java
+++ shop-qingyu-gateway/src/main/java/io/qingyu/shop/GatewayStarter.java
@@ -2,12 +2,14 @@ package io.qingyu.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author qingYu
  * @version 1.0.0
  * @Description 服务网关启动类
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayStarter {
     public static void main(String[] args) {
